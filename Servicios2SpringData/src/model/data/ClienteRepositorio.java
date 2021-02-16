4373a5dbd441980eee51464a1a6dbbba7526d776
Juan Manuel Gomez Gutierrez <78680491+juanma6969@users.noreply.github.com>
2021-02-16 09:41:56 +0100

    Add Comments
    Añaden comentarios explicativos sobre lo que se hace en algunas clases

--- Servicios2SpringData/src/model/data/ClienteRepositorio.java
+++ Servicios2SpringData/src/model/data/ClienteRepositorio.java
@@ -7,9 +7,26 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import dominio.Cliente;
+/**
+ *Podemos encontrar que ClienteRepositorio hereda de
+ * JpaReppository en vez de CrudReposiotory en vez de este ultimo 
+ * esto se debe basicamente CrudRepository tiene algunos metodos de busqueda, pero 
+ * por ejemplo paginacion no tiene , paginacion permite mayor velocidad a que extrae n Elementos de la 
+ * bbdd.Se comporta igual que CrudRepository<Clase a la que hace referencia,el tipo de valor de la clave primaria>
+ */
 
 @Repository
 public interface ClienteRepositorio extends JpaRepository<Cliente,Integer> {
+  /**
+   *Podemos realizar metodos de consulta personalizada , de dos maneras ya sea con @Query y indicando la consulta dentro de value
+   *para que no tome por defecto el lenguaje de consulta como jpql,debemos indicar que la consulta va a ser de forma true, con el 
+   *atributo nativeQuery=true;
+   *Pero spring nos ofrece crear consultas personalizadas siguiendo su sintaxis como norma siguiendo la siguiene forma:
+   *findBy/findAllBy/count entre alguno mas  + nombre de propiedad del dominio + aqui podemos utilizar algunos  como (StartingWith,Containing,EndingWith,IgnoreCase...) entre otros 
+   *+And/Or+nombre de otra propiedad del dominio + con lo mencionado anteriormente como  StartingWith ... + si queremos ordenarlo por alguna propiedad  + Asc/Desc;
+   *  Aqui podemos visualizar algunos ejemplos en el ServicioRepository podemos visualizar otros ejemplos .
+   *
+   */
 
 List<Cliente> findByNombreIgnoreCase(String nombre);
 List<Cliente> findByNombreIgnoreCaseAndEmailIgnoreCase(String nombre,String email);
