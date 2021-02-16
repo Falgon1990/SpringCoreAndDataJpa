7b21c0317c6d1faa116fc77979fe9a0501d487ad
Juan Manuel Gomez Gutierrez <78680491+juanma6969@users.noreply.github.com>
2021-02-16 10:13:35 +0100

    Add Comments
    Añade comentarios explicativos

--- Servicios2SpringData/src/configuracion/Configuracion.java
+++ Servicios2SpringData/src/configuracion/Configuracion.java
@@ -21,15 +21,32 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.google.common.base.Preconditions;
 
+/**
+ *Con respecto a los dos anteriores ejemplos, este presenta alguna modificacion si que podemos encontrar ,
+ * cierta similitud , con su version XML, pero por ejemplo ya no es una version de XML sino va a ser una  clase java 
+ * Va a ser menos verboso , tendremos anotaciones , un archivo de propiedades de proveeremos de cierta informacion al 
+ * la configuracion ,inidicaremos a spring que va a ser una clase de configuracion , donde se  va encontrar el archivo de propiedades
+ * lo paquetes que va escanear para indentificar los estereotipos , con reespecto a lo que la version de spring jpa 
+ * con spring data se deben indicar donde se van a encontrar los repositorio.
+ */
+
 @Configuration
 @PropertySource({"classpath:persistence-mysql.properties"})
 @ComponentScan(basePackages= {"model"})
 @EnableTransactionManagement
 @EnableJpaRepositories("model.data")
 public class Configuracion {
-
+	
+	/**
+	*Podemos ver que vamos a autoinyectar un tipo enviroment para acceder a los valores del archivo de properties
+	*Ademas encontramos el @Bean, se coloca a nivel de metodo, para que indifique que es un bean , como su version de xml 
+	* se indica cual va a ser su  vendorAdapter, entitimanager, datsource , alguna propiedad adicional que se encuentra en el archiv de 
+	* de propiedades , para que comprobar que el valor de propiedades , no es nulo , los vamos a importar de la api de google ,
+	* por medio del pom.xml gracias a maven .
+	**/
 	@Autowired
 	private Environment env;
+	
 	
 	 @Bean
 	 public PlatformTransactionManager transactionManager() {
